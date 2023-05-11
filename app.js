1f105a7ea99be21812cf4039f45e4ffab8691b68
Jahed Hossain <jahedev@gmail.com>
2023-05-11 15:26:56 -0400

    aws s3 connect, and temp file upload

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const pug = require('pug')
 const bodyParser = require('body-parser')
+var fileupload = require('express-fileupload')
 const port = 3000
 
 // --- MIDDLEWARE ---
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: 'tmp/',
+    safeFileNames: true,
+  })
+)
 app.set('view engine', 'pug')
 
 // --- ROUTES ---
--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -1,19 +1,57 @@
 const express = require('express')
 const router = express.Router()
 const aws = require('../lib/aws_client')
+const uuid = require('uuid')
+const fs = require('fs')
+const multer = require('multer')
 
 router.get('/', (req, res) => {
-  res.send(
-    `<h1>404 - Not Found</h1>
-    <p>You are supposed to send a POST request with the file through the form.</p>
-    `
-  )
+  res.render('error', {
+    title: '404 - Not Found',
+    message:
+      'You are supposed to send a POST request with the file through the form.',
+  })
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
+  // file upload error checking and restrictions
+  {
+    if (!req.files || !req.files.imageFile) {
+      res.render('error', {
+        title: '400 - Bad Request',
+        message: 'You did not upload an image using the form.',
+      })
+      return
+    }
+    const fileType = req.files.imageFile.mimetype.trim()
+    if (
+      fileType !== 'image/png' &&
+      fileType !== 'image/jpg' &&
+      fileType !== 'image/gif'
+    ) {
+      res.render('error', {
+        title: '400 - Bad Request',
+        message: 'That file extension is not supported.',
+      })
+      return
+    }
+  }
+
   const { imageName, imageDesc, imageFile } = req.body
   let uploadedUrl = 'N/A'
   let success = true
+
+  // console.log(form)
+
+  // const key = uuid.v4() + '.txt'
+  // console.log(key)
+  // console.log(req.headers['filename'])
+  // console.log(typeof imageFile)
+
+  // console.log(req)
+
+  // await aws.createObject(imageFile, key)
+  // await aws.listBuckets()
 
   // finally
   res.render('uploadImage', { success, imageName, imageDesc, uploadedUrl })
